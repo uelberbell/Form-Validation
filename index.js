20b43903cdbb9fdc449055a89d7ec0f4c9e66b47
uelberbell <72271381+uelberbell@users.noreply.github.com>
2022-10-26 17:32:45 -0300

    Update add function and CSS coolors
    Adicionando nova função setTimeout() e aplicando novas cores ao CSS.

--- index.js
+++ index.js
@@ -5,39 +5,32 @@ const aviso = document.querySelector(".aviso");
 const botao = document.querySelector(".btn");
 
 //Isso é uma function dentro do evento click
-botao.addEventListener("click", (enviar) => {
+botao.addEventListener("click", (enviar) => { // Essa é uma errowFunction, mas poderia ser feita com uma function normal.
   enviar.preventDefault();
-
-  const pessoa = {
-    nome: nome.value,
+  //Essa function poderia ser básica: function enviar(){}. Mas seria necessário chamar essa function no button com onclick ="enviar()"  
+  const pessoa = {                
+    nome: nome.value,             
     idade: idade.value,
     trabalha: seletor.value,
   };
 
-  console.log(
-    "Dados capturados pelo objeto:\n" +
-      "Nome: " +
-      pessoa.nome +
-      "\nIdade: " +
-      pessoa.idade +
-      "\nTrabalha: " +
-      pessoa.trabalha
-  );
-
   //Condicional para adicionar cores para user que nao preencher.
   if (nome.value === "" || idade.value === "") {
-    nome.style.border = "solid red 3px";
-    idade.style.border = " solid red 3px";
+    nome.style.border = "solid red 2px";
+    idade.style.border = " solid red 2px";
     aviso.textContent = "Precisa preencher os campos!";
+      console.log("Os dados não foram preenchidos pelo usuário!")
 
-    // Apos adcionar e validada as informacoes.
+      setTimeout(() => {//Removerá as transformações acima após 3 segundos.
+        aviso.textContent = ''
+        nome.style.border = ''
+        idade.style.border = ''
+      }, 3000);
+    // Apos adcionar e validar as informacoes.
   } else {
-    aviso.innerHTML = "";
-    nome.value = "";
-    idade.value = "";
+    nome.value = ""
+    idade.value = ""
     seletor.value = "";
-    nome.style.border = "none";
-    idade.style.border = "none";
     const resultado = document.querySelector(".resultado");
     resultado.innerHTML =
       "Dados capturados pelo objeto:\n" +
@@ -48,10 +41,26 @@ botao.addEventListener("click", (enviar) => {
       "\nTrabalha: " +
       pessoa.trabalha;
 
+        setTimeout(() =>{ //Removendo dados impressos no textArea.
+          resultado.innerHTML = ''
+        }, 3000)
+
+        //Estou adicionando esse log aqui para verificar os dados no console do navegador.
+      console.log(
+        "Dados capturados pelo objeto:\n" +
+          "Nome: " +
+          pessoa.nome +
+          "\nIdade: " +
+          pessoa.idade +
+          "\nTrabalha: " +
+          pessoa.trabalha
+      );
+
+
     //Criando uma tag li para entender o DOM. Isso não está sendo aprensentado na tela. Não criamos o Appendchild
     const li = document.createElement("li");
     li.classList = "item"; // Aqui é criada uma classe lá dentro do html usando o javascript. "class= "item""
     li.textContent = pessoa.nome;
-    console.log(li);
+    console.log(li); // Você pode observar esse dado sendo exibido no console do navegador.
   }
 });
